28455f3aab2623109f57db8bbbd0ff300a6a3bb6
kristiyan <kristiyan.tefov@icloud.com>
2023-05-30 19:01:29 +0200

    heroku fix

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
+const User = require("./User");
+const Blog = require("./Blog");
+const Comment = require("./Comment");
 
-const {User, Blog, Comment} = require("../models")
 
 
 User.hasMany(Blog, {
